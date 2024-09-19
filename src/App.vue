90397716f75f88279d4c0c8b44f26eb63eff055f
jianye <1321447500@qq.com>
2024-09-19 11:02:05 +0800

    feat: 修改全局页面x轴会出现滚动条问题

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <!-- <a-config-provider :locale="zhCN" :theme="theme"> -->
   <!-- <a-config-provider :locale="zhCN" :theme="{ algorithm: theme.darkAlgorithm }"> -->
   <a-config-provider :locale="zhCN" :theme="theme">
-    <Layout>
+    <Layout class="layout-overflow-x">
       <RouterView></RouterView>
     </Layout>
   </a-config-provider>
@@ -137,5 +137,8 @@
   /* 应用变量 */
   :is(body) {
     background-color: var(--primary-bg);
+  }
+  .layout-overflow-x {
+    overflow-x: hidden;
   }
 </style>
