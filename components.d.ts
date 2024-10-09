a005d97584eaeb9fbf27fa6c470c59f6ea0e185c
YingNing <2782409032@qq.com>
2024-10-09 14:04:13 +0800

    feat: 部分页面实现国际化功能

--- components.d.ts
+++ components.d.ts
@@ -31,6 +31,7 @@ declare module 'vue' {
     Animation: typeof import('./src/components/sql/input/animation.vue')['default']
     APopover: typeof import('ant-design-vue/es')['Popover']
     ARadio: typeof import('ant-design-vue/es')['Radio']
+    ARadioButton: typeof import('ant-design-vue/es')['RadioButton']
     ARadioGroup: typeof import('ant-design-vue/es')['RadioGroup']
     ARangePicker: typeof import('ant-design-vue/es')['RangePicker']
     ARow: typeof import('ant-design-vue/es')['Row']
